d6fef056a80ca08ac8535efefc06128d5a667d67
VitorshGF <victor.gomez2000@hotmail.com>
2020-12-28 16:19:03 -0600

    UserStock remove and add

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    stocks.where(id: stock.id).exists?
+    return false unless stock
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
