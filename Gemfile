98a4ffa3a105ce95282a325af93bec0550570e2b
VitorshGF <victor.gomez2000@hotmail.com>
2020-12-28 12:56:07 -0600

    Search using Ajax calls

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'devise-bootstrap-views'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'iex-ruby-client'
+gem "font-awesome-rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,10 @@ class Stock < ApplicationRecord
       secret_token: 'secret_token',
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price:client.price(ticker_symbol))
+    rescue
+      return nil
+    end
   end
 end
